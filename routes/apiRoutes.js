11948bb02e8556aeb23ceda3bb8646199c562831
wylie-u <wylieurig@gmail.com>
2021-03-21 16:03:19 -0600

    updating files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const shortid = require('shortid');
+//populates a unique id
+// console.log(shortid.generate());
+
+
+module.exports = app => {
+//  read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
+
+fs.readFile("/db/db.json", "utf8",( err, data) => {
+    if (err) throw err;
+    var note = JSON.parse(data);
+    console.log(data);
+    res.json(note);
+  });
+})
+//receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
+app.post('/api/notes', (req, res) => {
+  let savedNote = req.body;
+  note.push(savedNote);
+  //taking the data from the note variable 
+  fs.writeFile("db/db.json",JSON.stringify(note),err => {
+    if (err) throw err;
+    return true;
+});
+
+})
+
+// app.get('/api/notes/notes', (req, res) => {
+
+// })
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.use(express.json());
+
+require("./routes/htmlRoutes")(app);
+
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
