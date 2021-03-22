5b0f8cdafff30dc13497889e1c33602888c292f4
wylie-u <wylieurig@gmail.com>
2021-03-21 18:21:06 -0600

    updated unique id to req.body

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,13 +17,18 @@ fs.readFile("./db/db.json", "utf8",( err, data) => {
 })
 //receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
 app.post('/api/notes', (req, res) => {
-  let savedNote = req.body;
+
+  //setting req.body object to the uuid module to create a unique id for each note
+  req.body.id = uuidv4();
+  
   let note;
   // first read file, then push the saved note into note, which is a string of data
   fs.readFile("./db/db.json", "utf8", (err, data) => {
+    // JSON parse converts string into JSON objects
     note = JSON.parse(data);
-    console.log("Note Post: "+ note);
-    note.push(savedNote);
+    
+    // pushes the stored information from the req.body object into the note variable
+    note.push(req.body);
     // then writefile and return the response as json with the note (data)
     fs.writeFile("./db/db.json",JSON.stringify(note),err => {
       if (err) throw err;
